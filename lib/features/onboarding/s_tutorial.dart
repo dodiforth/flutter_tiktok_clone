b5e1f4c0b1fa70c331f613ae8c2a5ed18bd8c312
Dowon Kim <dowoniscool@yahoo.com>
2024-04-13 15:24:20 +0200

    Add Animation swipe functionality and create second page for Tutorial Screen

--- lib/features/onboarding/s_tutorial.dart
+++ lib/features/onboarding/s_tutorial.dart
@@ -1,6 +1,12 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_tiktok_clone/constants/gaps.dart';
 import 'package:flutter_tiktok_clone/constants/sizes.dart';
+
+enum Direction { right, left }
+
+enum Page { first, second }
 
 class TutorialScreen extends StatefulWidget {
   const TutorialScreen({super.key});
@@ -10,20 +16,47 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _TutorialScreenState extends State<TutorialScreen> {
+  Direction _direction = Direction.right;
+  Page _showingPage = Page.first;
+
+  void _onPanUpdate(DragUpdateDetails details) {
+    if (details.delta.dx > 0) {
+      setState(() {
+        _direction = Direction.right;
+      });
+    } else {
+      setState(() {
+        _direction = Direction.left;
+      });
+    }
+  }
+
+  void _onPanEnd(DragEndDetails details) {
+    if (_direction == Direction.left) {
+      setState(() {
+        _showingPage = Page.second;
+      });
+    } else {
+      setState(() {
+        _showingPage = Page.first;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 3,
+    return GestureDetector(
+      onPanUpdate: _onPanUpdate,
+      onPanEnd: _onPanEnd,
       child: Scaffold(
-        body: SafeArea(
-          child: TabBarView(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: Sizes.size12),
+            child: SafeArea(
+              child: AnimatedCrossFade(
+                firstChild: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Gaps.v36,
+                    Gaps.v40,
                     const Text(
                       "Watch cool videos!",
                       style: TextStyle(
@@ -41,15 +74,12 @@ class _TutorialScreenState extends State<TutorialScreen> {
                     ),
                   ],
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
+                secondChild: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Gaps.v36,
+                    Gaps.v40,
                     const Text(
-                      "Follow the rules!",
+                      "Create your own videos!",
                       style: TextStyle(
                         fontSize: Sizes.size40,
                         fontWeight: FontWeight.bold,
@@ -57,7 +87,7 @@ class _TutorialScreenState extends State<TutorialScreen> {
                     ),
                     Gaps.v16,
                     Text(
-                      "Videos are personalized based on what you watch, like, and share.",
+                      "Show your creativity and share y our life with the world.",
                       style: TextStyle(
                         fontSize: Sizes.size20,
                         color: Colors.grey.shade800,
@@ -65,51 +95,36 @@ class _TutorialScreenState extends State<TutorialScreen> {
                     ),
                   ],
                 ),
+                crossFadeState: _showingPage == Page.first
+                    ? CrossFadeState.showFirst
+                    : CrossFadeState.showSecond,
+                duration: const Duration(milliseconds: 300),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: Sizes.size24),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Gaps.v36,
-                    const Text(
-                      "Enjoy the ride",
-                      style: TextStyle(
-                        fontSize: Sizes.size40,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Gaps.v16,
-                    Text(
-                      "Videos are personalized based on what you watch, like, and share.",
-                      style: TextStyle(
-                        fontSize: Sizes.size20,
-                        color: Colors.grey.shade800,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
-          ),
-        ),
-        bottomNavigationBar: BottomAppBar(
-          surfaceTintColor: Colors.white,
-          elevation: 4,
-          child: Container(
-            padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                TabPageSelector(
-                  color: Colors.white,
-                  selectedColor: Theme.of(context).primaryColor,
-                )
-              ],
             ),
           ),
-        ),
-      ),
+          bottomNavigationBar: BottomAppBar(
+            surfaceTintColor: Colors.white,
+            elevation: 4,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                  vertical: Sizes.size3, horizontal: Sizes.size3),
+              child: AnimatedOpacity(
+                opacity: _showingPage == Page.second ? 1 : 0,
+                duration: const Duration(milliseconds: 300),
+                child: CupertinoButton(
+                  color: Theme.of(context).primaryColor,
+                  child: const Text(
+                    "Start the fun",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  onPressed: () {},
+                ),
+              ),
+            ),
+          )),
     );
   }
 }
